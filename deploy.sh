624314bd8be4dfc792fda256d4f7e7353eabb6eb
孙季 <sunji@playcrab.com>
2018-08-27 10:33:00 +0800

    rebuilding site 2018年 8月27日 星期一 10时33分00秒 CST

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ hugo -t beg # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd docs
-mv -f ./* ../  # ！！！！！删除已经存在的文件夹
+cp -rf ./* ../  # ！！！！！删除已经存在的文件夹
 cd ..
 # Add changes to git.
 git add .
